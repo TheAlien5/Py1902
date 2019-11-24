43a16a01b7067c16f52d4519d352d1e7d44298d2
Ivan <twerlader@gmail.com>
2019-11-24 09:46:52 +0000

    Changes to be committed:	new file:   03-11/__pycache__/math.cpython-37.pyc 	new file:   03-11/homework.py 	new file:   03-11/mathem.py 	new file:   new/lesson9.py 	new file:   new/logik.py

---
+++ 03-11/mathem.py
@@ -0,0 +1,17 @@
+import math # импортируем модуль math
+
+x = 3.265
+print(math.trunc(x))
+y = 11.72545645
+print(round(y, 2))
+print(round(y, 4))
+print(round(y, 18))
+
+
+
+
+print(math.pi)
+print(math.e)
+
+print(pow(x, 2))
+print(pow(x, 1 / 2))
---
+++ new/lesson9.py
@@ -0,0 +1,13 @@
+# import math
+# print(math.pi)
+
+# import math as
+# print(m.pi)
+
+# from math import
+# print(pi)
+
+from math import pi, cos, sin
+print(pi)
+y = 1 / cos(2)
+print(round(y,2))
